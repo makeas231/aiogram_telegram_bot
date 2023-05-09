ade91a377c90d34ec1878ec6e7a75303999802a9
makeas231 <gadafer231@gmail.com>
2023-05-09 16:19:34 +0300

    Добавил несколько изменений

--- src/keyboard.py
+++ src/keyboard.py
@@ -1,24 +1,35 @@
 from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-# Клавиатура для команды /start (основная клавиатура бота)
-keyboard_main_menu = ReplyKeyboardMarkup(resize_keyboard= True,
-                                         one_time_keyboard= True)
-kb1Start = KeyboardButton("/help")
-kb2Start = KeyboardButton("/download_video")
-kb3Start = KeyboardButton("/description")
-keyboard_main_menu.add(kb1Start, kb2Start, kb3Start)
 
-# Инлайн клавиатура, которая будет срабатывать после того как пользователь скачает видео
-inlineKeyboard_question = InlineKeyboardMarkup(row_width=2)
-kb1Like = InlineKeyboardButton(text= '👍🏻',
-                               callback_data = 'LikeAnswer')
-kb2Govno = InlineKeyboardButton(text= '💩',
-                               callback_data = 'GovnoAnswer')
+def main_keyboard() -> ReplyKeyboardMarkup:
+    """Основная клавиатура бота"""
+    keyboard_main_menu = ReplyKeyboardMarkup(resize_keyboard= True,
+                                            one_time_keyboard= True)
+    kb1Start = KeyboardButton("/help")
+    kb2Start = KeyboardButton("/download_video")
+    kb3Start = KeyboardButton("/description")
+    keyboard_main_menu.add(kb1Start, kb2Start, kb3Start)
+    
+    return keyboard_main_menu
 
-inlineKeyboard_question.add(kb1Like, kb2Govno)
+def download_video_ikb() -> InlineKeyboardMarkup:
+    """Инлайн клавиатура, которая будет срабатывать после того как пользователь скачает видео"""
+    inlineKeyboard_question = InlineKeyboardMarkup(row_width= 2)
+    kb1Like = InlineKeyboardButton(text= '👍🏻',
+                                callback_data = 'LikeAnswer')
+    kb2Govno = InlineKeyboardButton(text= '💩',
+                                callback_data = 'DislikeAnswer')
 
-# Кнопка "ОТМЕНА❌", которая будет появляться только когда работает команда download_video
-keyboard_cancel = ReplyKeyboardMarkup(resize_keyboard= True)
-kb1Cancel = KeyboardButton('ОТМЕНА❌')
-keyboard_cancel.add(kb1Cancel)
+    inlineKeyboard_question.add(kb1Like, kb2Govno)
+
+    return inlineKeyboard_question
+
+def keyboard_cancel_keyboard() -> ReplyKeyboardMarkup:
+    """Кнопка "ОТМЕНА❌", которая будет появляться только когда работает команда download_video"""
+    keyboard_cancel = ReplyKeyboardMarkup(resize_keyboard= True)
+    kb1Cancel = KeyboardButton('ОТМЕНА❌')
+    keyboard_cancel.add(kb1Cancel)
+
+    return keyboard_cancel
+    
--- src/main.py
+++ src/main.py
@@ -8,9 +8,10 @@ from aiogram.dispatcher import FSMContext
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher.filters.state import StatesGroup, State
 from aiogram.utils.exceptions import NetworkError
+from aiogram.dispatcher.filters import Text
 
-from keyboard import keyboard_main_menu, inlineKeyboard_question, keyboard_cancel
-from other import is_youtube_url, convert_time
+from keyboard import main_keyboard, download_video_ikb, keyboard_cancel_keyboard
+from other import is_youtube_url, convert_time, LIST_OF_ANSWER, HELP_TEXT, DESCTIPTION_TEXT, TEXT_INSTRUCTIONS
 from config import TOKEN_API
 
 
@@ -40,23 +41,17 @@ logger.addHandler(handler)
 # Словарь в котором будут храниться все активные пользователи(и видео которое они скачивают(ли))
 active_users = {}
 
-HELP_TEXT = f"""
-<b>/start</b> - Первоначальный запуск бота.
-<b>/help</b> - Помощь.
-<b>/description</b> - Описание бота.
-<b>/download_video</b> - Основной функционал бота, используя его можно скачивать видео.
-"""
-
-DESCTIPTION_TEXT = f"""
-Функционал данного бота максимально простой, используя команду /download_video
-ты можешь скачивать любое видео с Youtube. Если ты запутался, то можешь использовать
-команду /help, чтобы посмотреть все доступные команды.
-"""
 
 async def on_startup(_):
     """Асинхронная функция которая будет запускаться во время начала работы бота"""
     print("Телеграмм бот был успешно запущен!")
-                     
+
+@dp.message_handler(Text(equals= 'Привет!'))
+async def test_equals(message: types.Message):
+    """Тестовый фильтр, который будет обрабатывать именно текст
+    который будет отсылаться боту, а не команды"""
+    await message.answer("И тебе не хварать, друг")
+
 @dp.message_handler(commands=['start'])
 async def start_command(message: types.Message):
     """Хендлер для обработки команды /start"""
@@ -66,16 +61,17 @@ async def start_command(message: types.Message):
     # Если пользователь не ввел свое имя, присваиваем ему значение "Друг"
     if username == None:
         username = 'Друг'
-
+    
     START_TEXT = f"""Приветствую <b>{username}</b>
 С помощью данного телеграмм бота, ты сможешь скачивать бесплатно любые видео видео с Youtube.
 В случае ошибки, сразу пиши создателю бота - @makcum6"""
+
     await bot.send_sticker(chat_id=message.chat.id,
                         sticker='CAACAgIAAxkBAAEIf5NkMHhR_xuJLsLEKwP_g0QDaBLIXAACzhkAAlPHqEkTwUAIA_EBDy8E')
     await bot.send_message(chat_id=message.chat.id,
                         text=START_TEXT,
                         parse_mode='HTML',
-                        reply_markup= keyboard_main_menu)
+                        reply_markup= main_keyboard())
     await message.delete()
 
     logging.info(f"""{username} ----- Активировал функцию /start
@@ -85,7 +81,7 @@ async def start_command(message: types.Message):
 async def download_video(message: types.Message):
     await bot.send_message(chat_id= message.chat.id,
                         text='Чтобы скачать видео, скинь мне ссылку на ролик!',
-                        reply_markup= keyboard_cancel)
+                        reply_markup= keyboard_cancel_keyboard())
     await ProfileState.url_video.set() # Включаем состояние "url_video"
 
     logging.info(f"""{message.from_user.username} ----- Активировал функцию /download_video
@@ -97,34 +93,33 @@ async def url_video_state(message: types.Message, state: FSMContext):
     if url == 'ОТМЕНА❌':
         await bot.send_message(chat_id= message.chat.id,
                         text= "Выход в главное меню...",
-                        reply_markup= keyboard_main_menu)
+                        reply_markup= main_keyboard())
         await state.finish() # Если пользователь нажмет на кнопку ОТМЕНА❌, то возвращаемся в главное меню и выходим из состояния
     else:
-        try:
-            async with state.proxy() as data:
-                data['url_video'] = url
+        async with state.proxy() as data:
+            data['url_video'] = url
 
-            if is_youtube_url(url) == False:
-                await message.answer(f"""
+        if is_youtube_url(url) == False:
+            await message.answer(f"""
 URL - {url} ------> 
 Данная ссылка не ведет на сайт YouTube, попробуй ввести другую ссылку""")
-            else:                         
-                yt = YouTube(url)
-                await message.answer(text="Идет скачивание видео... Это может занять некоторое время👻")
-                video = yt.streams.get_highest_resolution()
+        else:                         
+            yt = YouTube(url)
+            await message.answer(text="Идет скачивание видео... Это может занять некоторое время👻")
+            video = yt.streams.get_highest_resolution()
 
-                # Название файла будет таким же как его название на YouTube
-                sent_video = f"{yt.title}.mp4" 
-                video.download(filename= sent_video)
+            # Название файла будет таким же как его название на YouTube
+            sent_video = f"{yt.title}.mp4" 
+            video.download(filename= sent_video)
 
-                # Добавляем пользователя скачавшего видео в словарь активных пользователй
-                active_users[f'last_down_video_{message.from_user.username}' ] = url
+            # Добавляем пользователя скачавшего видео в словарь активных пользователй
+            active_users[f'last_down_video_{message.from_user.username}' ] = url
             
-                logging.info(f"""{message.from_user.username} ----- начал скачивание видео {url}
+            logging.info(f"""{message.from_user.username} ----- начал скачивание видео {url}
 Активные пользователи на данный момент ----> {active_users}""")
                 
-                with open(sent_video, 'rb') as video_file:
-                    await bot.send_video(chat_id=message.chat.id,
+            with open(sent_video, 'rb') as video_file:
+                await bot.send_video(chat_id=message.chat.id,
                                         video=video_file,
                                         caption= f"""
 🎥 Название - {yt.title}
@@ -134,51 +129,20 @@ URL - {url} ------>
 👀 Просмотров - {yt.views}
 👮🏻‍♀️ Продолжительность - {convert_time(yt.length)}""")
 
-                os.remove(sent_video) # Удаляем скачанный файл после отправки
+            os.remove(sent_video) # Удаляем скачанный файл после отправки
 
-                await bot.send_message(chat_id = message.chat.id,
-                                        text= """Вот твое видео, а это инструкция для скачивания:
-<b>ДЛЯ ПК:</b>
-Нажми на видео которое тебе пришло, правой кнопкой мыши, и из предложенных тебе вариантов выбери
-<b>Save As...(Сохранить как)</b> После, у тебя откроется окно проводника,
-в котором тебе нужно будет сохранить видео в нужную тебе директорию.
-<b>ДЛЯ ТЕЛЕФОНОВ (Android, IOS):</b> 
-У видео которое тебе пришло, справа вверху будет три точки, нажми на них, и из всех вариантов выбери
-<b>Сохранить в галерею.</b>. И видео скачается тебе в галерею.""",
-                parse_mode= 'HTML',
-                reply_markup = keyboard_main_menu)
+            await bot.send_message(chat_id = message.chat.id,
+                                        text= TEXT_INSTRUCTIONS,
+            parse_mode= 'HTML',
+            reply_markup = main_keyboard())
                 # После завершения работы команды download_video, возвращаем кнопки главного меню
                 
-                logging.info(f"{message.from_user.username} ----- успешно скачал видео✅")
+            logging.info(f"{message.from_user.username} ----- успешно скачал видео✅")
 
-                await bot.send_message(chat_id= message.chat.id,
+            await bot.send_message(chat_id= message.chat.id,
                                         text="Спасибо что использовал нашего бота, как тебе сам бот?",
-                                        reply_markup= inlineKeyboard_question)
-                await state.finish()
-
-        except NetworkError as ne:
-            await bot.send_message(chat_id= message.chat.id,
-                                    text= """
-На данный момент времени, можно скачивать видео только до 30 мбит,
-к сожалению ваше видео превышает этот лимит, но не расстраивайся,
-это только тестовая часть бота :(, данный лимит будет расширен в скором времени!!!""",
-reply_markup= keyboard_main_menu)
+                                        reply_markup= download_video_ikb())
             await state.finish()
-
-            logging.info(f"""
-{message.from_user.username} ----- во время скачивание видео произошла ошибка❌  --- КОД ОШИБКИ:
-{ne}                         """)
-        except Exception as er:
-            await bot.send_message(chat_id= message.chat.id,
-                                        text= f"""
-Ой... Произошла не предвиденная ошибка :(
-Код ошибки ----> {er}, для решения проблемы напиши создателю @makcum6""",
-reply_markup= keyboard_main_menu)
-            await state.finish()
-
-            logging.info(f"""
-{message.from_user.username} ----- во время скачивание видео произошла ошибка❌ --- КОД ОШИБКИ:
-{er}""")
 
 @dp.message_handler(commands= ['help'])
 async def help_command(message: types.Message):
@@ -208,20 +172,8 @@ async def description_command(message: types.Message):
 async def noy_name_message(message: types.Message):
     """Хендлер для всех осталаных команд"""
     # Cписок ответов на неизвестные сообщения
-    list_of_answer = [
-"Извините, я не понимаю, о чем вы говорите. Пожалуйста, попробуйте сформулировать свой запрос по-другому.",
-"ЭЭЭЭ, начальника, я тебя не понимаю, активирую команду /help",
-"Простите, я не знаю, как на это ответить. Можете задать мне другой вопрос?",
-"К сожалению, я не обладаю достаточной информацией, чтобы ответить на ваше сообщение. Можете задать мне другой вопрос?",
-"Я умею отвечать только на определенные команды. Пожалуйста, введите одну из доступных команд или задайте мне вопрос по теме, на которую я специализируюсь.",
-"Извините, я не могу ответить на ваше сообщение в данный момент. Попробуйте позже или свяжитесь с моим разработчиком, чтобы уточнить причину проблемы.",
-"Пожалуйста, используйте одну из доступных команд или задайте мне вопрос по теме, которую я могу обработать. Список доступных команд можно получить, написав /help.",
-"Я не понимаю вашего сообщения. Пожалуйста, опишите свой запрос более подробно или свяжитесь с моим разработчиком, чтобы получить помощь.",
-"К сожалению, я не могу обработать ваш запрос. Попробуйте сформулировать его по-другому или задайте мне другой вопрос.",
-"Ой... Мне кажется я тебя не понимаю, для помощи, активируй команду /help",
-"Давай поговорим на другие темы? Посмотри в меню команды на которые я умею отвечать"]
     if message.text != ['/help', '/description', '/download_video', '/start']:
-        random_offer = random.choice(list_of_answer) # Достаем рандомное предложение из списка
+        random_offer = random.choice(LIST_OF_ANSWER) # Достаем рандомное предложение из списка
         await message.answer(text= random_offer)
 
     logging.info(f"""{message.from_user.username} ----- {message.text}
@@ -231,13 +183,10 @@ async def noy_name_message(message: types.Message):
 async def callback_data(callback: types.CallbackQuery):
     """Калбек хендлер, для обработки калбеков"""
     if callback.data == 'LikeAnswer':
-        await bot.send_sticker(chat_id= callback.message.chat.id,
-                               sticker='CAACAgIAAxkBAAEIl6FkOqw2RveQvmtnYbh9nh-9Ks3K3QACSSUAAsO1AUmg5-gksKmnmS8E')
-        await bot.send_message(chat_id= callback.message.chat.id,
-                               text= 'Спасибо :)')
-    if callback.data == 'GovnoAnswer':
-        await bot.send_message(chat_id= callback.message.chat.id,
-                               text= 'Очень жалко :(, если у тебя есть какие то заметки или идеи по улучшению бота, напиши разработчику @makcum6')
+        await callback.answer("Спасибо :)")
+    if callback.data == 'DislikeAnswer':
+        await callback.answer("Очень жалко :(, напиши разработчику @makcum6")
+
 
 if __name__ == '__main__':
     executor.start_polling(dispatcher= dp,
